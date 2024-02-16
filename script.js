7fe0f3894fbfd7a58479ac4a143a26c29c78ab05
MHALtd <hasbi1117@gmail.com>
2024-02-17 01:43:22 +0700

    fix offcanvas and page width issue

---
+++ script.js
@@ -0,0 +1,27 @@
+// COUNTDOWN SCRIPT
+simplyCountdown(".simply-countdown", {
+  year: 2024, // required
+  month: 3, // required
+  day: 28, // required
+  hours: 8, // Default is 0 [0-23] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "hari", plural: "hari" },
+    hours: { singular: "jam", plural: "jam" },
+    minutes: { singular: "menit", plural: "menit" },
+    seconds: { singular: "detik", plural: "detik" },
+  },
+});
+
+// FIX OFFCANVAS
+const hamburger = document.querySelector(".navbar-toggler");
+const stickyTop = document.querySelector(".sticky-top");
+const offCanvas = document.querySelector(".offcanvas");
+
+offCanvas.addEventListener("show.bs.offcanvas", function () {
+  stickyTop.style.overflow = "visible";
+});
+
+offCanvas.addEventListener("hidden.bs.offcanvas", function () {
+  stickyTop.style.overflow = "hidden";
+});
